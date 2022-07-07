f830b56a6026d9ae8b064477e1fc60a90884a782
MariaClaraSanchez <clarasanchez2398@gmail.com>
2022-07-07 18:12:33 -0300

    atualização dos exer feitos

--- js/scripts.js
+++ js/scripts.js
@@ -102,34 +102,34 @@ function exer4() {
         </p>
         `
     }
-    else{
+    else {
 
         custoFabrica = custoFabrica.substring(3);
         custoFabrica = custoFabrica.replaceAll('.', '');
         custoFabrica = custoFabrica.replaceAll(',', '.');
-    
+
         custoFabrica = parseFloat(custoFabrica);
-    
+
         let distribuidor = (28 * custoFabrica) / 100;
         let imposto = (45 * custoFabrica) / 100;
         let custo_total = custoFabrica + distribuidor + imposto;
-    
+
         custo_total = custo_total.toFixed(2);
         custo_total = custo_total.replaceAll('.', ',');
-        
+
         document.getElementById("e4").innerHTML = `
         <p>O custo final do consumidor é : R$ ${custo_total}
         </p>
         `
     }
-    
+
     limpar();
 }
 
 //Exercício 5
 function exer5() {
-    salario = document.getElementById("salario").value;
-    reajuste = document.getElementById("reajuste").value;
+    let salario = document.getElementById("salario").value;
+    let reajuste = document.getElementById("reajuste").value;
 
     if (salario == "" || reajuste == "") {
         document.getElementById("e5").innerHTML = `
@@ -137,19 +137,19 @@ function exer5() {
         </p>
         `
     }
-    else if(reajuste>100 || reajuste<0){
+    else if (reajuste > 100 || reajuste < 0) {
         alert("Valor não pode ser maior que menor que 0% e nem maior 100%");
     }
-    else{
+    else {
 
         salario = salario.substring(3);
         salario = salario.replaceAll('.', '');
         salario = salario.replaceAll(',', '.');
         salario = parseFloat(salario);
 
-        reajuste = parseFloat(reajuste);        
-        salario += (salario* (reajuste/100));
-        
+        reajuste = parseFloat(reajuste);
+        salario += (salario * (reajuste / 100));
+
         salario = salario.toFixed(2);
         salario = salario.replaceAll('.', ',');
 
@@ -163,7 +163,147 @@ function exer5() {
 
 //Exercício 6
 function exer6() {
-    
+    let n1 = parseFloat(document.getElementById("n1").value);
+    let n2 = parseFloat(document.getElementById("n2").value);
+    let n3 = parseFloat(document.getElementById("n3").value);
+
+
+    if (!n1 || !n2 || !n3) {
+        document.getElementById("e6").innerHTML = `
+        <p class="text-danger">Por favor digite um dos valores!!
+        </p>
+        `
+    }
+    else {
+        let media = (n1 + n2 + n3) / 3;
+        console.log(media);
+        document.getElementById("e6").innerHTML = `<p>A média das notas é ${media}</p>`
+    }
+
+}
+
+//Exercício 7
+function exer7() {
+    let salario = document.getElementById("salario_hora").value;
+    let horas = document.getElementById("horas_trabalhadas").value;
+
+    if (!salario || !horas) {
+        document.getElementById("e7").innerHTML = `
+        <p class="text-danger">Por favor digite um dos valores!!
+        </p>
+        `
+    }
+    else if (horas < 0) {
+        alert("Hora não pode ser maior que menor que 0!");
+    }
+    else {
+
+        salario = salario.substring(3);
+        salario = salario.replaceAll('.', '');
+        salario = salario.replaceAll(',', '.');
+        salario = parseFloat(salario);
+
+        horas = parseFloat(horas);
+        let salario_regular = (salario * 160); //150 - por mês
+        let hora_extra = horas - 160;
+        let salario_final = 0;
+
+        if (hora_extra >= 0) {
+            salario_final = salario_regular + (hora_extra * (salario + 0.5));
+        }
+        else {
+            salario_final = salario_regular;
+        }
+
+        salario_final = salario_final.toFixed(2);
+        salario_final = salario_final.replaceAll('.', ',');
+
+        document.getElementById("e7").innerHTML = `
+        <p>O salário final é : <b> R$ ${salario_final} </b>
+        </p>
+        `
+    }
+    limpar();
+}
+
+//Exercício 8
+function exer8() {
+    let salario, vendas, total = 0, diferenca = 0;
+
+    salario = document.querySelector("#salario").value;
+    vendas = document.querySelector("#vendas").value;
+
+    if (!salario || !vendas) {
+        document.getElementById("e8").innerHTML = `
+        <p class="text-danger">Por favor digite um dos valores!!
+        </p>
+        `
+    }
+    else {
+        salario = salario.substring(3);
+        salario = salario.replaceAll('.', '');
+        salario = salario.replaceAll(',', '.');
+        salario = parseFloat(salario);
+
+        vendas = vendas.substring(3);
+        vendas = vendas.replaceAll('.', '');
+        vendas = vendas.replaceAll(',', '.');
+        vendas = parseFloat(vendas);
+
+        if (vendas <= 1500) {
+            total = salario + (0.03 * vendas);
+            console.log(0.03 * vendas);
+        } else {
+            diferenca = vendas - 1500;
+            total = salario + (0.03 * 1500) + (0.05 * diferenca);
+        }
+
+        document.getElementById("e8").innerHTML = `<p>O salário será igual a R$ ${total}</p>`
+    }
+
+}
+
+
+//Exercício 9
+function exer9() {
+    const gasolina = 3.30;
+    const alcool = 2.90;
+
+    let litro = document.getElementById("litros").value;
+    let tipo = document.getElementById("tipo").value;
+
+    if (!litro || !tipo) {
+        document.getElementById("e9").innerHTML = `
+        <p class="text-danger">Por favor digite um dos valores!!
+        </p>
+        `
+    }
+    else {
+        if (litro <= 20) {
+            if (tipo == "Gasolina") {
+                valor_pago = (litro * 0.04) * gasolina;
+            }
+            else {
+                valor_pago = (litro * 0.03) * alcool;
+            }
+        }
+        else {
+            if (tipo == "Gasolina") {
+                valor_pago = (litro * 0.06) * gasolina;
+            }
+            else {
+                valor_pago = (litro * 0.04) * alcool;
+            }
+        }
+        valor_pago = valor_pago.toFixed(2);
+        valor_pago = valor_pago.replaceAll('.', ',');
+
+        document.getElementById("e9").innerHTML = `
+        <p>O valor pago foi : <b> R$ ${valor_pago} </b>
+        </p>
+        `
+    }
+
 }
 
 
@@ -174,5 +314,8 @@ function limpar() {
 (function () {
     VMasker(document.getElementById("ex4")).maskMoney({ unit: 'R$' });
     VMasker(document.getElementById("salario")).maskMoney({ unit: 'R$' });
+    VMasker(document.getElementById("salario_hora")).maskMoney({ unit: 'R$' });
+    VMasker(document.getElementById("salario8")).maskMoney({ unit: 'R$' });
+    VMasker(document.getElementById("vendas")).maskMoney({ unit: 'R$' });
 })();
 
